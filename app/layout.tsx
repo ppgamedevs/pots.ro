21357ffceec36e5e551bbf77a319ab1b553a12bd
ppgamedevs <ppgamedevs@gmail.com>
2025-12-15 18:41:20 +0200

    feat: Integrate Google Tag Manager for enhanced tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -151,6 +151,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ro" className={`${interTight.variable} ${merriweather.variable}`} suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PMLWCKXG');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-EEMSRXZBG7"></script>
         <script
@@ -168,6 +182,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
       </head>
       <body className="font-sans antialiased text-[#1A1A1A] bg-[#FAFAF8] min-h-screen">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PMLWCKXG"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider>
           <ConfirmProvider>
             <ErrorBoundary>
