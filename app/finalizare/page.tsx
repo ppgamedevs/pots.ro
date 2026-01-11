0f23b761e16abebb558c0eac25bc0cb25ccf09d3
ppgamedevs <ppgamedevs@gmail.com>
2026-01-12 00:49:14 +0200

    fix: Add București to county options in finalizare page for completeness

--- app/finalizare/page.tsx
+++ app/finalizare/page.tsx
@@ -24,7 +24,7 @@ const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 const countyOptions = [
   "Alba", "Arad", "Argeș", "Bacău", "Bihor", "Bistrița-Năsăud", "Botoșani",
-  "Brăila", "Brașov", "Buzău", "Călărași", "Caraș-Severin", "Cluj", "Constanța",
+  "Brăila", "Brașov", "București", "Buzău", "Călărași", "Caraș-Severin", "Cluj", "Constanța",
   "Covasna", "Dâmbovița", "Dolj", "Galați", "Giurgiu", "Gorj", "Harghita",
   "Hunedoara", "Ialomița", "Iași", "Ilfov", "Maramureș", "Mehedinți", "Mureș",
   "Neamț", "Olt", "Prahova", "Sălaj", "Satu Mare", "Sibiu", "Suceava",
